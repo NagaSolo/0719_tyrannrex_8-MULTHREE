0b7516ae88abcf03f4934500bcbbffd09fa5c808
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-07-19 17:12:47 +0100

    add: brainstorming scratch

---
+++ multhree.py
@@ -0,0 +1,15 @@
+''' good chance to make use of python generator
+
+T -> testcases
+d[0] -> first element
+d[1] -> second element
+d[k-1] -> last element
+K -> len(d[k-1])
+
+input (if T=1):
+T
+K d[0] d[1]
+
+output (if T=1):
+YES or NO
+'''
