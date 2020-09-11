5d8ab6fba888508726aecf101b28bf7523305c88
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-11 16:23:40 +0100

    added; luhn generator, partially correct

--- multhree.py
+++ multhree.py
@@ -12,4 +12,17 @@ K d[0] d[1]
 
 output (if T=1):
 YES or NO
+
 '''
+
+def generate_luhn(dn, d0, d1):
+    num_list = [d0, d1]
+    while len(num_list) < dn:
+        num_list.append(sum(num_list))
+    return num_list
+
+# driver
+if __name__ == "__main__":
+    test = [[5, 3, 4], [13, 8, 1], [760399384224, 5, 1]]
+    for t in test:
+        print(generate_luhn(t[0], t[1], t[2]))
